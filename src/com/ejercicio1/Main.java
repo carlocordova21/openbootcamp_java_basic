e59a6b6ca0ea378f840c598768d802789ba25a4c
carlocordova21 <carlo_andre21@hotmail.com>
2022-07-22 12:25:15 -0500

    Ejercicio 1 completado

---
+++ src/com/ejercicio1/Main.java
@@ -0,0 +1,21 @@
+package com.ejercicio1;
+
+/**
+ * Clase principal punto de entrada a la aplicación
+ * @author cacoridev
+ */
+public class Main {
+    public static void main(String[] args) {
+        int numInt = 10;
+        long numLong = 12312321L;
+        double numDouble = 12312.42;
+        boolean state = true;
+        String cadena = "Hola mundo";
+
+        System.out.println(numInt);
+        System.out.println(numLong);
+        System.out.println(numDouble);
+        System.out.println(state);
+        System.out.println(cadena);
+    }
+}
